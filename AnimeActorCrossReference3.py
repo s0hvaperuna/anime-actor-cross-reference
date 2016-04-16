3e3575431c70b2a527ccd5d5809967f980750fbb
s0hvaperuna <tariqharb54@gmail.com>
2016-04-16 12:05:01 +0300

    You can now select watching, completed, etc. on their own.

--- AnimeActorCrossReference3.py
+++ AnimeActorCrossReference3.py
@@ -3,31 +3,67 @@ import sys
 import pprint
 import requests
 import lxml
+import time
 from bs4 import BeautifulSoup
 from threading import Thread
 
+
 def split(a, n):
-    k, m = int(len(a) / n), len(a) % n
+    le = len(a)
+    if n > le:
+        n = le
+    k, m = int(le / n), le % n
     sliced = []
-    for i in range(n):
-        sliced += [a[i * k:(i + 1) * k]]
-    sliced += [a[-m]]
+    if k > 0:
+        for i in range(n):
+            sliced += [a[i * k:(i + 1) * k]]
+    if m > 0:
+        sliced += [a[-m:]]
     return sliced
 
-def main():
-    usr = input("MAL username: ")
-    f = input("Save to file (y/n): ")
 
-    r = requests.get('http://myanimelist.net/malappinfo.php?u=' + str(usr) + '&status=all&type=anime', headers={"user-agent": "iMAL-iOS"})
+def main():
+    usr = 's0hvaperuna'#input("MAL username: ")
+    f = 'n'#input("Save to file (y/n): ")
+    s = 'a'#input("Filter\na(all), w(watching), c(completed), o(onhold), d(dropped), p(plan to watch)\nYou can combine different categories e.g. wd would get all from watching and dropped\n")
+
+    r = requests.get('http://myanimelist.net/malappinfo.php?u=' + str(usr) + '&status=all&type=anime',
+                     headers={"user-agent": "iMAL-iOS"})
 
     soup = BeautifulSoup(r.text, "lxml-xml")
-    titles = soup.find_all('series_title')
-    ids = soup.find_all('series_animedb_id')
 
-    total_watched = int(soup.find('user_watching').contents[0]) + int(soup.find('user_completed').contents[0]) +\
-            int(soup.find('user_onhold').contents[0]) + int(soup.find('user_dropped').contents[0]) +\
-            int(soup.find('user_plantowatch').contents[0])
+    if 'a' in s:
+        titles = soup.find_all('series_title')
+        ids = soup.find_all('series_animedb_id')
+        total_watched = int(soup.find('user_watching').contents[0]) + int(soup.find('user_completed').contents[0]) + \
+                        int(soup.find('user_onhold').contents[0]) + int(soup.find('user_dropped').contents[0]) + \
+                        int(soup.find('user_plantowatch').contents[0])
+    else:
+        status = []
+        titles = []
+        ids = []
+        total_watched = 0
+        if 'w' in s:
+            status += [1]
+            total_watched += int(soup.find('user_watching').contents[0])
+        if 'c' in s:
+            status += [2]
+            total_watched += int(soup.find('user_completed').contents[0])
+        if 'o' in s:
+            status += [3]
+            total_watched += int(soup.find('user_onhold').contents[0])
+        if 'd' in s:
+            status += [4]
+            total_watched += int(soup.find('user_dropped').contents[0])
+        if 'p' in s:
+            status += [6]
+            total_watched += int(soup.find('user_plantowatch').contents[0])
 
+        all_titles = soup.find_all('anime')
+        for title in all_titles:
+            if int(title.find('my_status').contents[0]) in status:
+                titles += [title.find('series_title')]
+                ids += [title.find('series_animedb_id')]
     counter = 0
     master = {}
 
@@ -71,26 +107,32 @@ def main():
         indexes += [i]
 
     threads = []
-    index = split(indexes, 15)
-    for idx, group in enumerate(split(urls, 15)):
+    thread = 15 # Max amount of full sized threads. Usually there will be one more smaller thread
+    index = split(indexes, thread)
+    t1 = time.time()
+    for idx, group in enumerate(split(urls, thread)):
         t = Thread(target=get, args=(group, index[idx],))
         t.start()
         threads += [t]
-
     for t in threads:
         t.join()
-
     sys.stdout.write('\r')
+    print('\n', time.time() - t1)
     display(master, f)
 
+
 def display(dict, f):
-    f = sys.stdout
     if f == 'y' or f == 'Y':
         f = open('voice_actor_cross_reference.txt', 'w')
+    else:
+        f = sys.stdout
     for key in dict:
         print(key, file=f)
         for j in dict[key]:
             print('    ' + j[0] + ' - ' + j[1], file=f)
-    f.close()
+
+
 if __name__ == "__main__":
+    t = time.time()
     main()
+    print('Total time: ', time.time() - t)
